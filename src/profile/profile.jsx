386d8e0fdad97ba2935c22d8b5b118e5ed31e16c
Morgan Miles <mgmiles7@byu.edu>
2026-02-12 19:32:25 -0700

    created profile component

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import "./profile.css"
 
 export function Profile() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>profile displayed here</div>
-    </main>
+    <div className="main">
+      <img src="../logo.png" alt="Us logo"/>
+            <h2>Morgan</h2>
+            <p id = 'name'>Time with Rebecca: 5 months</p>
+            <div>
+            <button className="btn btn-primary send" type="submit">Idea please!</button>
+            <button className="btn btn-outline-primary" type="submit"><a id="log-out" href="index.html">Log out</a></button>
+            </div>
+    </div>
   );
 }
