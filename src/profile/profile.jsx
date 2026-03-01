9103687c9ad10176e1c61e8671108b8a05a0fbd5
Morgan Miles <mgmiles7@byu.edu>
2026-02-28 17:17:33 -0700

    added functionality to show and hide api idea

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -4,11 +4,15 @@ import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import { AuthState } from '../login/authState';
 export function Profile(props) {
+  const [showApi, setAPI] = React.useState(false);
   const navigate = useNavigate();
   function logout(){
     localStorage.removeItem('username');
     props.onAuthChange(null, AuthState.Unauthenticated)
     navigate("/");
+  }
+  function apiHandler() {
+    setAPI(!showApi);
   }
 
   return (
@@ -17,9 +21,13 @@ export function Profile(props) {
             <h2 id = "user">{props.user.username}</h2>
             <p id = 'name'>{`Time with ${props.user.with}`}: 5 months</p>
             <div>
-            <button className="btn btn-primary send" type="submit">Idea please!</button>
+            <button className="btn btn-primary send" onClick={() => apiHandler()}>Idea please!</button>
             <button className="btn btn-outline-primary" onClick={() => logout()} >Log-out</button>
             </div>
+            {showApi &&
+              <div>
+                Try growing a bonsai tree!
+              </div>}
     </div>
   );
 }
