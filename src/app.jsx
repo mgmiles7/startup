4806bb11055079044d8153bb9d5c6326ba43ce9e
Morgan Miles <mgmiles7@byu.edu>
2026-03-11 23:13:22 -0600

    lifting post state

--- src/app.jsx
+++ src/app.jsx
@@ -10,11 +10,7 @@ import { AuthState } from './login/authState';
 import { User } from './user';
 
 export default function App() {
-    // const [userName, setUserName] = React.useState(localStorage.getItem('username') || "");
-    // const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
-    // const [authState, setAuthState] = React.useState(currentAuthState)
-    // const [linked, changeLink] = React.useState({state: false, id: userName});
-
+    
     const [user, setUser] = React.useState(() => {
         const savedUser = localStorage.getItem('user');
 
@@ -27,6 +23,9 @@ export default function App() {
     const [authState, setAuthState] = React.useState(currentAuthState);
     const [linked, changeLink] = React.useState(false);
     const [chatActive, setChatActive] = React.useState(false);
+    
+    const [post, setPost] = React.useState([]);
+    
     
 
     function ProtectedRoutes({ authState, children}){
@@ -50,7 +49,8 @@ export default function App() {
         linked={linked}
         changeLink={changeLink}
         chatActive={chatActive}
-        setChatActive={setChatActive} />
+        setChatActive={setChatActive}
+        />
     )
 
     return ( 
@@ -67,7 +67,9 @@ export default function App() {
                     linked={linked}
                     changeLink={changeLink}
                     chatActive={chatActive}
-                    setChatActive={setChatActive} />} 
+                    setChatActive={setChatActive}
+                    post={post}
+                    setPost={setPost} />} 
                 />
                 <Route path='Chat' element={<Chat 
                     user={user}
@@ -75,9 +77,11 @@ export default function App() {
                     linked={linked}
                     changeLink={changeLink}
                     chatActive={chatActive}
-                    setChatActive={setChatActive} />}
+                    setChatActive={setChatActive} 
+                    post={post}
+                    setPost={setPost}/>}
                 />
-                <Route path = 'Timeline' element={<Timeline user= {user} setUser = {setUser} linked={linked} changeLink={changeLink}/>} />
+                <Route path = 'Timeline' element={<Timeline user= {user} setUser = {setUser} linked={linked} changeLink={changeLink} post={post} setPost={setPost}/>} />
                 <Route path = 'Profile' 
                     element={<Profile 
                         user = {user}
